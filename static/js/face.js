7ef5636a2baaa48e5c02e31f5ab8b9c3e623b80d
Rohit Singh <rohitjsingh974@gmail.com>
2021-12-14 23:46:07 +0530

    add camera to test page

--- static/js/face.js
+++ static/js/face.js
@@ -18,15 +18,21 @@ function startVideo() {
 
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
+  // document.body.append(canvas)
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    // console.log("detections "+Objects.keys(detections))
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
+    console.log(resizedDetections.length)
+    if(resizedDetections.length>1){
+      console.log("too many people")
+    }
+    else if(resizedDetections.length==0) console.log("no one")
+    // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    // faceapi.draw.drawDetections(canvas, resizedDetections)
     //  faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
+  }, 1000)
 })
